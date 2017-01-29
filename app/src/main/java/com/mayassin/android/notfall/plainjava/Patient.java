122a4126797c6f9e2a8b919f4b601e91b9787ab1
Anurag Akkiraju <anurag.akkiraju@gmail.com>
2017-01-28 22:13:56 -0600

    hmm

--- app/src/main/java/com/mayassin/android/notfall/plainjava/Patient.java
+++ app/src/main/java/com/mayassin/android/notfall/plainjava/Patient.java
@@ -1,8 +1,78 @@
 package com.mayassin.android.notfall.plainjava;
 
-/**
- * Created by mohamed on 1/28/17.
- */
+
 
 public class Patient {
+
+  String username;
+  int age;
+  String fullName;
+  String insuranceProvider;
+  double latitude;
+  double longitude;
+  String primaryCaretaker;
+
+  public Patient(username, age, fullName, insuranceProvider, latitude, longitude, primaryCaretaker){
+
+  }
+
+  public String getUsername(){
+
+  }
+
+  public int getAge(){
+
+  }
+
+  public String getFullName(){
+
+  }
+
+  public String getInsuranceProvider(){
+
+  }
+
+  public double getLatitude(){
+
+  }
+
+  public double getLongitude(){
+
+  }
+
+  public String getPrimaryCaretaker(){
+
+  }
+
+
+
+  public String setUsername(){
+
+  }
+
+  public int getAge(){
+    
+  }
+
+  public String getFullName(){
+
+  }
+
+  public String getInsuranceProvider(){
+
+  }
+
+
+  public double getLatitude(){
+
+  }
+
+  public double getLongitude(){
+
+  }
+
+  public String getPrimaryCaretaker(){
+
+  }
+
 }
